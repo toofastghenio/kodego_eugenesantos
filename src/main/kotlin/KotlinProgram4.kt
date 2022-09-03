ff071dbbceaa3b6920ba7dfdb91f2d25ecf95aed
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-03 15:19:13 +0800

    first

---
+++ src/main/kotlin/KotlinProgram4.kt
@@ -0,0 +1,16 @@
+fun main (){
+    var number : Int = 10
+    if(number < 10 ){
+         println("Hello")
+
+    }
+
+    var grade : Int = 75
+    if(grade >=75 ) {
+        println("you passed!")
+
+    }else{
+        println("you failed")
+
+    }
+}
