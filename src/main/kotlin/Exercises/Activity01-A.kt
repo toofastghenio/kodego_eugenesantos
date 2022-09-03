2142c7a30b1f2bb0ac77fb86ec7953bd8142a25f
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-03 16:31:05 +0800

    first

---
+++ src/main/kotlin/Exercises/Activity01-A.kt
@@ -0,0 +1,32 @@
+package exerciseActivity
+
+
+fun main (){
+    // students assignments
+    var man: String = "Sir"
+    var woman: String = "Mam"
+
+    //students firstname
+    var student1: String = "Eugene"
+    var student2: String = "Paola"
+    var student3: String = "Ghenio"
+
+    //students grade
+    var student1Grade: Double = 95.00
+    var student2Grade: Double = 85.00
+    var student3Grade: Double = 80.00
+
+    // students attendance
+    var student1attendance: Int = 100
+    var student2attendance: Int = 90
+    var student3attendance: Int = 74
+
+    println("Hello, $man $student1! Your grade is $student1Grade")
+    println("Hello, $woman $student2! Your grade is $student2Grade")
+    println("Hello, $man $student3! Your grade is $student3Grade")
+
+
+
+
+}
+
---
+++ src/main/kotlin/Exercises/Activity01-B.kt
@@ -0,0 +1,18 @@
+package exerciseActivity
+
+
+fun main (){
+   var book1 = "Atlas"
+    var book2 = "World History"
+    var book3 = "Asia History"
+
+  var status1 = "borrowed"
+    var status2 = "returned"
+
+    println("The book $book1 is $status2")
+    println("The book $book2 is $status1")
+    println("The book $book3 is $status1")
+
+
+
+}
