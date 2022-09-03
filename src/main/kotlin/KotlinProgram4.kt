b6638c79acc814f93d396681dc43cf091091ed8e
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-03 15:47:56 +0800

    first

--- src/main/kotlin/KotlinProgram4.kt
+++ src/main/kotlin/KotlinProgram4.kt
@@ -1,16 +1,23 @@
 fun main (){
-    var number : Int = 10
-    if(number < 10 ){
-         println("Hello")
+    var grade : Int = 100
 
+    if((grade >=96)&&(grade <=100)) {
+        println("1.00")
+    }else if ((grade >=86)&&(grade <=95)){
+        println("1.75")
+    }    else if ((grade >=76)&&(grade <=85)) {
+        println("2.25")
+    }    else if(grade <75 ){
+        println("5.0")
+    }    else {
+        println("Invalid grade!")
     }
-
-    var grade : Int = 75
-    if(grade >=75 ) {
-        println("you passed!")
-
-    }else{
-        println("you failed")
-
+        var gender : String = "X"
+        if(gender == "M"){
+            println("Male")
+        }else if(gender == "F"){
+            println("Female")
+        }else{
+            println("Invalid choice")
+            }
     }
-}
