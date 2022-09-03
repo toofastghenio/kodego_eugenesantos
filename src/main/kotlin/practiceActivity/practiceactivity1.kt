90ae0d2a208d035253c9da54c7146390c39b7231
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-03 12:49:43 +0800

    practiceactivity1

---
+++ src/main/kotlin/practiceActivity/practiceactivity1.kt
@@ -0,0 +1,17 @@
+package practiceActivity
+
+
+fun main (){
+    println("Enter your firstName:")
+    var firstName : String = readln()
+
+    println("Enter your Middle Name/Initial")
+    var middleName : String = readln()
+
+    println("Enter your Lastname:")
+    var lastName : String =readln()
+
+    println("Your Name is $firstName $middleName $lastName")
+
+
+}
