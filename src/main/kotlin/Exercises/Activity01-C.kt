4fcf21bd8cd8770df8eb9b1fe85178c6798164ab
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-03 17:02:24 +0800

    first

---
+++ src/main/kotlin/Exercises/Activity01-C.kt
@@ -0,0 +1,43 @@
+package exerciseActivity
+
+
+fun main (){
+    //Food Products
+    var foodCategory1 : String = "Dry Goods"
+    var foodCategory2 : String = "Wet Goods"
+    var foodCategory3 : String = "Dairy Goods"
+
+    //Food Type
+    var foodType1 : String = "Canned Goods"
+    var foodType2 : String = "Meat Products"
+    var foodType3 : String = "Milk"
+
+    //Food Price
+    var foodPrice1 : Double = 20.00
+    var foodPrice2 : Double = 320.00
+    var foodPrice3 : Double = 120.00
+
+    //Food Stocks
+    var foodStocks1 : Int = 150
+    var foodStocks2 : Int = 50
+    var foodStocks3 : Int = 300
+
+    //Availability
+    var food1 : Boolean = foodStocks1 > 0
+    var food2 : Boolean = foodStocks2 > 0
+    var food3 : Boolean = foodStocks3 > 0
+
+    //total amounts
+    var amount1 : Double = foodPrice1 * foodStocks1
+    var amount2 : Double = foodPrice2 * foodStocks2
+    var amount3 : Double = foodPrice3 * foodStocks3
+
+    println("The $foodCategory1 is worth $foodPrice1 pesos and the are $foodStocks1 pcs available.")
+    println("with total amount of $amount1 pesos.")
+    println(food1)
+
+
+
+
+
+}
