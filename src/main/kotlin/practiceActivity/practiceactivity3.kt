036058ae0086b6381beb7eb798652e9ce71f65c7
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-03 17:19:55 +0800

    activity3

---
+++ src/main/kotlin/practiceActivity/practiceactivity3.kt
@@ -0,0 +1,15 @@
+fun main () {
+    var age: Int = 31
+
+    if ((age >= 1) && (age <= 17)) {
+        println("underage")
+    } else if ((age >= 18) && (age <= 30)) {
+        println("young adult")
+    } else if ((age >= 31) && (age <= 59)) {
+        println("adult")
+    } else if (age >= 60) {
+        println("senior")
+    }else{
+    println("Invalid age")}
+
+}
