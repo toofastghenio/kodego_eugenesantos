c54e467084412b26ffa7526531713dc715405295
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-05 19:39:36 +0800

    firstcommit

---
+++ src/main/kotlin/KotlinProgram8.kt
@@ -0,0 +1,12 @@
+fun main(){
+    //while loop
+    //init
+    var ctr : Int = -5
+    //condition
+    while(ctr <=5){
+        println("Hello World! $ctr")
+        //update
+        ctr++
+    }
+
+}
