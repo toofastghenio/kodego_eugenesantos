c1510684a92575315ecf36ec8dc0f2a8a013d06e
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-05 18:07:53 +0800

    activity01E

---
+++ src/main/kotlin/Exercises/Activity01-E.kt
@@ -0,0 +1,16 @@
+package exercises
+
+
+fun main(){
+
+
+
+    println("Please enter in your Fullname:")
+    var fullName1 : String = readln().toString()
+
+    println("Please enter in your Job")
+    var dayJob1 : String = readln().toString()
+
+    println("Your name is $fullName1. Presently working as $dayJob1")
+
+}
