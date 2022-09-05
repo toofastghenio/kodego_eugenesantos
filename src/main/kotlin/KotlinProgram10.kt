5f59f5af051b4ee53987041a84ac9d064e790518
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-05 20:11:08 +0800

    firstcommit

---
+++ src/main/kotlin/KotlinProgram10.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var ctr1 : Int = 1
+    var ctr2 : Int = 1
+    while(ctr1 <= 5){                   //@@@@@
+         var ctr2 : Int = 1             //@@@@@
+         while(ctr2 <=5){               //@@@@@
+             print("@")                 //@@@@@
+             ctr2++                     //@@@@@
+         }
+         println()
+        ctr1++
+    }
+}
+
