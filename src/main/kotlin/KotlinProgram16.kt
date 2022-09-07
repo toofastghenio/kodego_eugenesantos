15aa70b939519aff4e79a1bdea1a5daee377bfc6
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-07 20:45:59 +0800

    updated

---
+++ src/main/kotlin/KotlinProgram16.kt
@@ -0,0 +1,13 @@
+fun main(){
+    var names = ArrayList<String>()
+    names.add("John")
+    names.add("Peter")
+    //add elements
+    println(names[0])
+    println(names[1])
+    // remove elements
+    names.removeAt(0) //Peter -> index 0
+    //display names[0]
+    println(names[0])
+
+}
