610bed420debb4d48d1fb9cb3150b74de6755aff
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-07 20:38:51 +0800

    updated

---
+++ src/main/kotlin/KotlinProgram15.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var num = arrayOf(
+        //0        1        2
+        arrayOf<String>("Peter","Paul","Mary"), //0
+        arrayOf<String>("John","Mark","Matthew"), //1
+        arrayOf<String>("James","Luke","Zachary"), //2
+    )
+
+    var row : Int = 0
+    while(row<3){
+        var column : Int = 0
+        while (column <3){
+        print("${num[row][column]} ")
+        column++
+    }
+    println()
+    row++
+
+    }
+}
