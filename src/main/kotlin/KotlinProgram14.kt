c4f47f19cf988e5df828ec5d27cf8db6590a454b
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-07 20:21:36 +0800

    updated

---
+++ src/main/kotlin/KotlinProgram14.kt
@@ -0,0 +1,23 @@
+fun main(){
+
+    var names = arrayOf<String> ("John","Paul","Mark","Ringo","George")
+    var ctr : Int = 0
+    //change array values
+    while(ctr < names.size){
+        println("Enter new value for index $ctr")
+        (names[ctr]) = readln()
+
+        ctr++
+    }
+    //display new array values
+    var ctr2 : Int = 0
+    while(ctr2 < names.size){
+        println(names[ctr2])
+
+        ctr2++
+    }
+
+
+
+
+}
