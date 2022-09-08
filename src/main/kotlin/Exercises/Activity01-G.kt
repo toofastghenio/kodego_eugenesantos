4e85e81ccc86926b294204ef2dc1f009fe82a5a1
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-09 01:24:15 +0800

    updated

--- src/main/kotlin/Exercises/Activity01-G.kt
+++ src/main/kotlin/Exercises/Activity01-G.kt
@@ -1,3 +1,27 @@
 package exercises
 //Activity 01 - G
 //Create an application that will accept 2 Integers. It will identify the lower value and bigger value of the two inputs. Then, your application will print all the prime numbers starting from the lower number up to the largest number.
+
+//# Kotlin program to display all the prime numbers within an interval
+
+fun main(args: Array<String>) {
+    var low = 0
+    val high = 100
+
+    while (low < high) {
+        var riders = false
+
+        for (s in 2..low / 2) {
+            // condition for nonprime number
+            if (low % s == 0) {
+                riders = true
+                break
+            }
+        }
+
+        if (!riders)
+            print("$low ")
+
+        ++low
+    }
+}
