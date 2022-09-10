dfabfa09d6aa2c8018112f36cde73d6ec80be980
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-10 11:23:10 +0800

    updated

---
+++ src/main/kotlin/KotlinProgram21.kt
@@ -0,0 +1,30 @@
+fun main(){
+
+    var myObject = MyClass("Hello World")
+
+}
+
+class MyClass(value: String){
+    init{
+        println(value)
+    }
+}
+
+
+
+
+
+/*
+Class -> objects
+object -> variable
+       -> functions(process)
+uppercase -> first letter
+examples.
+String
+Int
+
+
+
+
+
+ */
