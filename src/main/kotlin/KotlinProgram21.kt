e3c21cb2cf753106bcd8c6fe65f49a3358234126
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-10 11:44:37 +0800

    updated

--- src/main/kotlin/KotlinProgram21.kt
+++ src/main/kotlin/KotlinProgram21.kt
@@ -1,12 +1,21 @@
 fun main(){
 
-    var myObject = MyClass("Hello World")
+    //instantiation - process of creating an object
+    var myObject = MyClass("Hello World!")
+    var myObject2 = MyClass("hi World!")
+    var myGenericObject = MyGenericClass( true)
 
 }
-
+//Generics
+class MyGenericClass<T>(value:T ){
+    init{
+        println(value.toString().length)
+    }
+}
 class MyClass(value: String){
     init{
-        println(value)
+        println(value.length)
+
     }
 }
 
