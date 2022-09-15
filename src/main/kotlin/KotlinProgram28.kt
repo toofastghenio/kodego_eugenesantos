7d52c1c99e6f281de3df66e35de10baac977ded6
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-15 21:10:38 +0800

    updated

---
+++ src/main/kotlin/KotlinProgram28.kt
@@ -0,0 +1,33 @@
+fun main(){
+    var myCar = Car()
+
+    myCar.fuelVolume = -100
+    println(myCar.fuelVolume)
+}
+class Car{
+    var modelName:String = ""
+        get() = field
+        set(value){
+            field = value
+        }
+    var fuelVolume:Int = 0
+        get() = field
+        set(value){
+            if(value < 0){
+                println("invalid value")
+            }else{
+                field = value
+            }
+        }
+    init{
+        println("Object Created!")
+        println("modelName is $modelName")
+        println("fuelVolume is $fuelVolume")
+    }
+    fun accelerate(fuelVolume: Int){
+        this.fuelVolume = this.fuelVolume - fuelVolume
+    }
+    fun checkfuel(){
+        println("Current fuel Volume is $fuelVolume")
+    }
+}
