a02e3667afb8194133218b4138d4a7fea2ef7f3b
Eugene santos <evsantos.filigreesales@gmail.com>
2022-09-15 18:29:01 +0800

    updated

--- src/main/kotlin/KotlinProgram26.kt
+++ src/main/kotlin/KotlinProgram26.kt
@@ -1,11 +1,13 @@
-fun main() {
+fun main(){
+
     //lambda
-    var sum: (Int, Int) -> Int = { num1, num2 ->
+    var sum : (Int, Int) -> Int = { num1, num2 ->
         num1 + num2
     }
+    println(sum(2,3))
+}
 
-    println(sum(2, 3))
-}
-fun sum(num1: Int, num2: Int):Int {
-    return num1 + num2
-}
+//fun sum(num1: Int, num2: Int):Int{
+//
+//    return num1 + num2
+//}
